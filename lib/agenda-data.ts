e7ad17c3ca2808d19cf93053d60053ee7d7e49c0
Xinran Waibel <xinran.waibel@gmail.com>
2026-04-02 21:43:27 -0700

    Update Frontiers of Data speakers

--- lib/agenda-data.ts
+++ lib/agenda-data.ts
@@ -404,8 +404,14 @@ export const sessions: Session[] = [
         title: "Chief Strategy Officer & Apache Airflow PMC",
         company: "Astronomer",
         linkedin: "https://www.linkedin.com/in/vikramkoka/",
-        avatarUrl: null,
-        bio: "Coming soon",
+        avatarUrl: "/speakers/Vikram-Koka.jpg",
+      },
+      {
+        name: "Laura Pruitt",
+        title: "Director, Data Science & Engineering",
+        company: "Netflix",
+        linkedin: "https://www.linkedin.com/in/lpruitt/",
+        avatarUrl: "/speakers/Laura-Pruitt.jpeg",
       },
     ],
     moderator: {
